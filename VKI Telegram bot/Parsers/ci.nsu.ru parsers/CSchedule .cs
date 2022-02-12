306ebf941b08da2c32bcbfc0801a97cc4d1dfa14
NicholasValentain <87443329+NicholasValentain@users.noreply.github.com>
2022-02-13 02:12:26 +0700

    set

---
+++ VKI Telegram bot/Parsers/ci.nsu.ru parsers/CSchedule .cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VKI_Telegram_bot.Parsers.ci.nsu.ru_parsers
+{
+    public class CSchedule : Parser
+    {
+        public List<string[]> returned = new List<string[]>();
+        public CSchedule(string url = "https://ci.nsu.ru/education/raspisanie-zvonkov/") : base(url)
+        {
+            updater();
+        }
+        public void updater()
+        {
+            returned.Clear();
+            //doc.DocumentNode.SelectSingleNode(".//table[@class='table']");
+            //class="table"
+        }
+    }
+}
--- VKI Telegram bot/Program.cs
+++ VKI Telegram bot/Program.cs
@@ -4,11 +4,31 @@ using static VKI_Telegram_bot.Parsers.ci.nsu.ru_parsers.PDFP;
 
 var timetable = new PDFP("https://ci.nsu.ru/education/schedule/");
 var sgroup = new PDFP("https://ci.nsu.ru/education/spisok-uchebnykh-grupp/");
+var cschedule = new CSchedule();
 foreach (var i in timetable.returned)
 {
     Console.WriteLine($"{ i[0] }\n{ i[1] }");
 }
+Console.WriteLine("\n");
 foreach (var i in sgroup.returned)
 {
     Console.WriteLine($"{ i[0] }\n{ i[1] }");
+}
+Console.WriteLine("\n");
+foreach(var i in cschedule.doc.DocumentNode.SelectSingleNode(".//table[@class='table']").SelectNodes(".//tr"))
+{
+    try 
+    {
+        foreach (var j in i.SelectNodes(".//th"))
+        {
+            Console.WriteLine(j.GetAttributeValue("p", ""));
+        }
+    }
+    catch (Exception ex)
+    {
+        foreach (var j in i.SelectNodes(".//td"))
+        {
+            Console.WriteLine(j.GetAttributeValue("p", ""));
+        }
+    }
 }
