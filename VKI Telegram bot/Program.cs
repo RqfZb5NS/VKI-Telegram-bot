bc1f04d58c3767a8d633e3ba3a178270a9fa3b0a
Hiter67 <rerwrqrtry@gmail.com>
2022-02-14 00:03:14 +0700

    rjgf.cm d ыйдшеу

--- VKI Telegram bot/Program.cs
+++ VKI Telegram bot/Program.cs
@@ -29,8 +29,28 @@ using VKI_Telegram_bot.Parsers.ci.nsu.ru_parsers;
 //{
 //    Console.WriteLine($"{ i[0] }\n{ i[1] }");
 //}
-using (var connection = new SqliteConnection("Data Source=usersdata.db"))
+using (var connection = new SqliteConnection("Data Source=DataBase.db"))
 {
     connection.Open();
+    SqliteCommand command = new SqliteCommand();
+    command.Connection = connection;
+    command.CommandText = "INSERT INTO Users (id, admin) VALUES (1, 1)";
+    using (SqliteDataReader reader = command.ExecuteReader())
+    {
+        if (reader.HasRows) // если есть данные
+        {
+            while (reader.Read())   // построчно считываем данные
+            {
+                var id = reader.GetValue(0);
+                var admin = reader.GetValue(1);
+
+                Console.WriteLine($"{id} \t {admin} \t ");
+            }
+        }
+        else
+        {
+            Console.WriteLine("данных нет");
+        }
+    }
 }
 Console.Read();
