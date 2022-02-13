286b4fd931375ded40084cc61e0249ca4d9bd7d5
NicholasValentain <87443329+NicholasValentain@users.noreply.github.com>
2022-02-13 15:28:46 +0700

    Парсинг пересдач и зачётов

--- VKI Telegram bot/Program.cs
+++ VKI Telegram bot/Program.cs
@@ -3,6 +3,7 @@ using VKI_Telegram_bot.Parsers.ci.nsu.ru_parsers;
 
 var timetable = new PDFP("https://ci.nsu.ru/education/schedule/");
 var sgroup = new PDFP("https://ci.nsu.ru/education/spisok-uchebnykh-grupp/");
+var iertification = new PDFP("https://ci.nsu.ru/education/raspisanie-ekzamenov/");
 var cschedule = new CSchedule();
 foreach (var i in timetable.returned)
 {
@@ -21,4 +22,9 @@ foreach(var i in cschedule.returned)
         Console.Write($"{j} ");
     }
     Console.WriteLine();
+}
+Console.WriteLine("\n");
+foreach (var i in iertification.returned)
+{
+    Console.WriteLine($"{ i[0] }\n{ i[1] }");
 }
