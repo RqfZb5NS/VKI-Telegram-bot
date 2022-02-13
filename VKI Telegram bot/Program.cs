555e94c2e0dd0cff4d4817624e9549cf631555eb
Hiter67 <rerwrqrtry@gmail.com>
2022-02-13 15:23:42 +0700

    small updates

--- VKI Telegram bot/Program.cs
+++ VKI Telegram bot/Program.cs
@@ -1,6 +1,5 @@
 ﻿using VKI_Telegram_bot.Parsers;
 using VKI_Telegram_bot.Parsers.ci.nsu.ru_parsers;
-using static VKI_Telegram_bot.Parsers.ci.nsu.ru_parsers.PDFP;
 
 var timetable = new PDFP("https://ci.nsu.ru/education/schedule/");
 var sgroup = new PDFP("https://ci.nsu.ru/education/spisok-uchebnykh-grupp/");
