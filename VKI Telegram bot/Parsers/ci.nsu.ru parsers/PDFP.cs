2d80afd487f5b5632ba3f9481bf07a6ad4c90cd6
Hiter67 <rerwrqrtry@gmail.com>
2022-02-13 23:39:18 +0700

    add db

--- VKI Telegram bot/Parsers/ci.nsu.ru parsers/PDFP.cs
+++ VKI Telegram bot/Parsers/ci.nsu.ru parsers/PDFP.cs
@@ -8,17 +8,17 @@ namespace VKI_Telegram_bot.Parsers.ci.nsu.ru_parsers
 {
     public class PDFP : Parser
     {
-        public List<string[]> returned = new List<string[]>();
+        public List<string[]> list = new List<string[]>();
         public PDFP(string url) : base(url)
         {
             updater();
         }
         public void updater()
         {
-            returned.Clear();
+            list.Clear();
             foreach (HtmlAgilityPack.HtmlNode i in doc.DocumentNode.SelectNodes(".//div[@class='file-div']"))
             {
-                returned.Add(new string[] { i.SelectSingleNode(".//div[@class='file-name']").InnerText.Trim(), "https://ci.nsu.ru" + i.SelectSingleNode(".//a").GetAttributeValue("href", "") });
+                list.Add(new string[] { i.SelectSingleNode(".//div[@class='file-name']").InnerText.Trim(), "https://ci.nsu.ru" + i.SelectSingleNode(".//a").GetAttributeValue("href", "") });
             }
         }
     }
--- VKI Telegram bot/Program.cs
+++ VKI Telegram bot/Program.cs
@@ -1,30 +1,36 @@
-﻿using VKI_Telegram_bot.Parsers;
+﻿using Microsoft.Data.Sqlite;
+using VKI_Telegram_bot.Parsers;
 using VKI_Telegram_bot.Parsers.ci.nsu.ru_parsers;
 
-var timetable = new PDFP("https://ci.nsu.ru/education/schedule/");
-var sgroup = new PDFP("https://ci.nsu.ru/education/spisok-uchebnykh-grupp/");
-var iertification = new PDFP("https://ci.nsu.ru/education/raspisanie-ekzamenov/");
-var cschedule = new CSchedule();
-foreach (var i in timetable.returned)
+//var timetable = new PDFP("https://ci.nsu.ru/education/schedule/");
+//var sgroup = new PDFP("https://ci.nsu.ru/education/spisok-uchebnykh-grupp/");
+//var iertification = new PDFP("https://ci.nsu.ru/education/raspisanie-ekzamenov/");
+//var cschedule = new CSchedule();
+//foreach (var i in timetable.list)
+//{
+//    Console.WriteLine($"{ i[0] }\n{ i[1] }");
+//}
+//Console.WriteLine("\n");
+//foreach (var i in sgroup.list)
+//{
+//    Console.WriteLine($"{ i[0] }\n{ i[1] }");
+//}
+//Console.WriteLine("\n");
+//foreach(var i in cschedule.list)
+//{
+//    foreach (var j in i)
+//    {
+//        Console.Write($"{j} ");
+//    }
+//    Console.WriteLine();
+//}
+//Console.WriteLine("\n");
+//foreach (var i in iertification.list)
+//{
+//    Console.WriteLine($"{ i[0] }\n{ i[1] }");
+//}
+using (var connection = new SqliteConnection("Data Source=usersdata.db"))
 {
-    Console.WriteLine($"{ i[0] }\n{ i[1] }");
+    connection.Open();
 }
-Console.WriteLine("\n");
-foreach (var i in sgroup.returned)
-{
-    Console.WriteLine($"{ i[0] }\n{ i[1] }");
-}
-Console.WriteLine("\n");
-foreach(var i in cschedule.returned)
-{
-    foreach (var j in i)
-    {
-        Console.Write($"{j} ");
-    }
-    Console.WriteLine();
-}
-Console.WriteLine("\n");
-foreach (var i in iertification.returned)
-{
-    Console.WriteLine($"{ i[0] }\n{ i[1] }");
-}
+Console.Read();
