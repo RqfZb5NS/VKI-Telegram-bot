bb3eb641f417ef993958e7dfe10d6249c2e24867
Hiter67 <rerwrqrtry@gmail.com>
2022-02-14 10:48:58 +0700

    small update

--- VKI Telegram bot/Program.cs
+++ VKI Telegram bot/Program.cs
@@ -3,33 +3,33 @@ using VKI_Telegram_bot.Parsers;
 using VKI_Telegram_bot.Parsers.ci.nsu.ru_parsers;
 using VKI_Telegram_bot.DB;
 
-//var timetable = new PDFP("https://ci.nsu.ru/education/schedule/");
-//var sgroup = new PDFP("https://ci.nsu.ru/education/spisok-uchebnykh-grupp/");
-//var iertification = new PDFP("https://ci.nsu.ru/education/raspisanie-ekzamenov/");
-//var cschedule = new CSchedule();
-//foreach (var i in timetable.list)
-//{
-//    Console.WriteLine($"{ i[0] }\n{ i[1] }");
-//}
-//Console.WriteLine("\n");
-//foreach (var i in sgroup.list)
-//{
-//    Console.WriteLine($"{ i[0] }\n{ i[1] }");
-//}
-//Console.WriteLine("\n");
-//foreach(var i in cschedule.list)
-//{
-//    foreach (var j in i)
-//    {
-//        Console.Write($"{j} ");
-//    }
-//    Console.WriteLine();
-//}
-//Console.WriteLine("\n");
-//foreach (var i in iertification.list)
-//{
-//    Console.WriteLine($"{ i[0] }\n{ i[1] }");
-//}
+var timetable = new PDFP("https://ci.nsu.ru/education/schedule/");
+var sgroup = new PDFP("https://ci.nsu.ru/education/spisok-uchebnykh-grupp/");
+var iertification = new PDFP("https://ci.nsu.ru/education/raspisanie-ekzamenov/");
+var cschedule = new CSchedule();
+foreach (var i in timetable.list)
+{
+    Console.WriteLine($"{ i[0] }\n{ i[1] }");
+}
+Console.WriteLine("\n");
+foreach (var i in sgroup.list)
+{
+    Console.WriteLine($"{ i[0] }\n{ i[1] }");
+}
+Console.WriteLine("\n");
+foreach (var i in cschedule.list)
+{
+    foreach (var j in i)
+    {
+        Console.Write($"{j} ");
+    }
+    Console.WriteLine();
+}
+Console.WriteLine("\n");
+foreach (var i in iertification.list)
+{
+    Console.WriteLine($"{ i[0] }\n{ i[1] }");
+}
 //var usertable = new UsersTable();
 //usertable.DelUser(2);
 //foreach (var i in usertable.GetUserList())
