10d1539de2d6b26a4a93311cda16d88922791c0d
Hiter67 <rerwrqrtry@gmail.com>
2022-03-18 19:29:48 +0700

    Доделал бд с json

---
+++ VKI Telegram bot/Parsers/ci.nsu.ru parsers/PDFPs/Sgroup.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VKI_Telegram_bot.DB;
+
+namespace VKI_Telegram_bot.Parsers.ci.nsu.ru_parsers.PDFPs
+{
+    public class Sgroup : PDFP
+    {
+        public Sgroup(string url = "https://ci.nsu.ru/education/spisok-uchebnykh-grupp/", 
+            string name = "sgroup") : base(url, name)
+        {
+
+        }
+    }
+}
--- VKI Telegram bot/Program.cs
+++ VKI Telegram bot/Program.cs
@@ -2,31 +2,24 @@
 using VKI_Telegram_bot.Parsers;
 using VKI_Telegram_bot.DB;
 using Microsoft.Extensions.Configuration;
-using VKI_Telegram_bot.Parsers.ci.nsu.ru_parsers.PDFPs;
+using VKI_Telegram_bot.Parsers.ci.nsu.ru_parsers;
+using System.Text.Json;
 
 var builder = new ConfigurationBuilder(); // установка пути к текущему каталогу
 builder.SetBasePath(Directory.GetCurrentDirectory()); // получаем конфигурацию из файла appsettings.json
 builder.AddJsonFile("appsettings.json"); // создаем конфигурацию
 var config = builder.Build(); // получаем строку подключения
-//Console.WriteLine(config.GetSection("TG").GetSection("Key").Value);
+                              //Console.WriteLine(config.GetSection("TG").GetSection("Key").Value);
 
-//var timetable = new Timetables();
-//Console.WriteLine(timetable.Update());
+var iertification = new PDFP("https://ci.nsu.ru/education/raspisanie-ekzamenov/", "iertification");
+var sgroup = new PDFP("https://ci.nsu.ru/education/spisok-uchebnykh-grupp/", "sgroup");
+var timetable = ("https://ci.nsu.ru/education/schedule/", "timetable");
+var cschedule = new CSchedule();
 
-//var iertification = new Iertifications();
-//Console.WriteLine(iertification.Update());
-
-var sgroup = new Sgroups();
-Console.WriteLine(sgroup.Update());
-
-foreach (var i in sgroup.list)
+using (VKITGBContext db = new VKITGBContext())
 {
-    Console.WriteLine($"{ i[0]}\n{ i[1] }");
+    foreach (var i in JsonSerializer.Deserialize<List<List<string>>>(db.Dates.Find("timetable").JSonData))
+    {
+        Console.WriteLine($"{ i[0]}\n{ i[1] }");
+    }
 }
-
-//foreach (var i in timetable.list)
-//{
-//    Console.WriteLine($"{ i.Name}\n{ i.Link }");
-//}
-
-//var cschedule = new CSchedule();
